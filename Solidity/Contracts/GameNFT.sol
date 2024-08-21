ce448711f66452e3e1d990b5aa99856f8fb2a5a2
Promise Emmanuel <95595405+Promise-csharp@users.noreply.github.com>
2024-08-21 06:22:57 +0100

    Update GameNFT.sol

---
+++ Solidity/Contracts/GameNFT.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract GameNFT is ERC721, Ownable {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    // Struct to hold NFT metadata
+    struct NFTMetadata {
+        string name;
+        string symbol;
+        string uri;
+    }
+
+    
+}
